97f5a7bf830d367931ed29b9e8fe7b989e568415
AbdelrhamanTareq <abdo_tarek01@yahoo.com>
2023-05-14 19:47:31 +0300

    adjust app localiztaion

--- lib/app.dart
+++ lib/app.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:muslim_app/features/home/presentation/views/home_view.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -15,27 +14,29 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
-        
       ),
       home: const Directionality(
           textDirection: TextDirection.rtl, child: HomwView()),
-        locale: const Locale("ar"),
-        localizationsDelegates: const [
-                   
-                    GlobalMaterialLocalizations.delegate,
-                    GlobalWidgetsLocalizations.delegate,
-                    GlobalCupertinoLocalizations.delegate,
-                  ],
-                  localeResolutionCallback: (deviceLocale, supportedLocales) {
-                    for (var locale in supportedLocales) {
-                      if (deviceLocale != null &&
-                          deviceLocale.languageCode == locale.languageCode) {
-                        return deviceLocale;
-                      }
-                    }
+      locale: const Locale("ar"),
+      supportedLocales: const [
+        Locale("ar"),
+        Locale("en"),
+      ],
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      localeResolutionCallback: (deviceLocale, supportedLocales) {
+        for (var locale in supportedLocales) {
+          if (deviceLocale != null &&
+              deviceLocale.languageCode == locale.languageCode) {
+            return deviceLocale;
+          }
+        }
 
-                    return supportedLocales.first;
-                  },
+        return supportedLocales.first;
+      },
     );
   }
 }
