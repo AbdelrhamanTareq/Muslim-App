4ec265f112927a3e5ebcc3842102471d0b164896
AbdelrhamanTareq <abdo_tarek01@yahoo.com>
2023-05-14 19:17:32 +0300

    remove debug banner and add localiztion

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:muslim_app/features/home/presentation/views/home_view.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -9,12 +11,31 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Muslim',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        
       ),
       home: const Directionality(
           textDirection: TextDirection.rtl, child: HomwView()),
+        locale: const Locale("ar"),
+        localizationsDelegates: const [
+                   
+                    GlobalMaterialLocalizations.delegate,
+                    GlobalWidgetsLocalizations.delegate,
+                    GlobalCupertinoLocalizations.delegate,
+                  ],
+                  localeResolutionCallback: (deviceLocale, supportedLocales) {
+                    for (var locale in supportedLocales) {
+                      if (deviceLocale != null &&
+                          deviceLocale.languageCode == locale.languageCode) {
+                        return deviceLocale;
+                      }
+                    }
+
+                    return supportedLocales.first;
+                  },
     );
   }
 }
