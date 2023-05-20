e9a940f13db88155acdd5ffe6ec6aa9a84278040
AbdelrhamanTareq <abdo_tarek01@yahoo.com>
2023-05-20 21:24:26 +0300

    adjust surhs ui

--- lib/features/quran/presentation/view/quran_surahs_view.dart
+++ lib/features/quran/presentation/view/quran_surahs_view.dart
@@ -14,6 +14,7 @@ class _QuranSurahsViewState extends State<QuranSurahsView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xffedebf2),
       body: SafeArea(child: BlocBuilder<QuranCubit, QuranState>(
         builder: (context, state) {
           if (state is QuranGetAllDataError) {
@@ -24,39 +25,60 @@ class _QuranSurahsViewState extends State<QuranSurahsView> {
             return ListView.separated(
               itemCount: state.data.length,
               itemBuilder: (context, index) {
-                return InkWell(
-                  onTap: () => Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) =>  QuranSurahDetails(state.data[index].array),
-                      )),
-                  child: ListTile(
-                    leading: Text(
-                      state.data[index].name,
-                      style: const TextStyle(
+                return Container(
+                  // padding: EdgeInsets.all(4),
+                  // color: index % 2 == 0
+                  //     ? Color.fromARGB(255, 245, 224, 151)
+                  //     : Color.fromARGB(255, 248, 217, 103
+                  //   )
+                  // ,
+                  child: InkWell(
+                    onTap: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              QuranSurahDetails(state.data[index].array, state.data[index].name),
+                        )),
+                    child: ListTile(
+                      leading: Text(
+                        state.data[index].name,
+                        style: const TextStyle(
                           fontFamily: "me_quran",
                           fontSize: 22,
                           //fontWeight: FontWeight.bold
-                          ),
-                    ),
-                    trailing: FittedBox(
-                      child: Column(
-                        children: [
-                          const Text(
-                            "عدد آياتها",
-                            style: TextStyle(
-                              fontFamily: "me_quran",
-                              fontSize: 20,
+                        ),
+                      ),
+                      trailing: FittedBox(
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                const Text(
+                                  " عدد آياتها",
+                                  style: TextStyle(
+                                    fontFamily: "me_quran",
+                                    fontSize: 20,
+                                  ),
+                                ),
+                                const SizedBox(
+                                  width: 8,
+                                ),
+                                Text(state.data[index].array.length.toString(),
+                                    style: const TextStyle(
+                                      fontFamily: "me_quran",
+                                      fontSize: 20,
+                                    ))
+                              ],
                             ),
-                          ),
-                          Text(
-                            (state.data[index].array.length).toString(),
-                            style: const TextStyle(
-                              fontFamily: "Uthman",
-                              fontSize: 22,
+                            Text(
+                              (state.data[index].type).toString(),
+                              style: const TextStyle(
+                                fontFamily: "Uthman",
+                                fontSize: 22,
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
                   ),
