5d9fb182997549fa80441da7eaac711685ccd822
AbdelrhamanTareq <abdo_tarek01@yahoo.com>
2023-06-26 10:38:04 +0300

    add click sound and edit app asets class

--- lib/core/utils/app_assets.dart
+++ lib/core/utils/app_assets.dart
@@ -1,35 +1,41 @@
-const String assetsPath = "assets/";
-const String quranPath = "assets/quran";
-const String hadithPath = "assets/hadith";
-const String azkarPath = "assets/azkar";
-const String imageAssetsPath = "assets/images";
+class AppAssets {
+  AppAssets._();
 
-abstract class AppAssets {
-  static const quran = "$quranPath/quran.json";
+  static const String _assetsPath = "assets/";
+  static const String _quranPath = "$_assetsPath/quran";
+  static const String _hadithPath = "$_assetsPath/hadith";
+  static const String _azkarPath = "$_assetsPath/azkar";
+  static const String _imageAssetsPath = "$_assetsPath/images";
+  static const String _soundsAssetsPath = "$_assetsPath/sounds";
   //////////////////////////////////////////////////////
-  static const azkar = "$azkarPath/azkar.json";
-  static const mainAzkar = "$azkarPath/main_azkar.json";
+
+  static const quran = "$_quranPath/quran.json";
   //////////////////////////////////////////////////////
-  static const bukhariPath = "$hadithPath/bukhari.json";
-  static const muslimPath = "$hadithPath/muslim.json";
-  static const ahmadPath = "$hadithPath/ahmad.json";
-  static const nasaiPath = "$hadithPath/nasai.json";
-  static const malikPath = "$hadithPath/malik.json";
-  static const darimiPath = "$hadithPath/darimi.json";
-  static const tirmidziPath = "$hadithPath/tirmidzi.json";
-  static const abuDaudPath = "$hadithPath/abu-daud.json";
-  static const ibnuMajahPath = "$hadithPath/ibnu-majah.json";
+  static const azkar = "$_azkarPath/azkar.json";
+  static const mainAzkar = "$_azkarPath/main_azkar.json";
   //////////////////////////////////////////////////////
-  static const quranSvgPath = "$imageAssetsPath/quran.svg";
-  static const compassSvgPath = "$imageAssetsPath/compass.svg";
-  static const hadithSvgPath = "$imageAssetsPath/hadith.svg";
-  static const seb7aSvgPath = "$imageAssetsPath/seb7a.svg";
-  static const alarmSvgPath = "$imageAssetsPath/alarm.svg";
+  static const bukhariPath = "$_hadithPath/bukhari.json";
+  static const muslimPath = "$_hadithPath/muslim.json";
+  static const ahmadPath = "$_hadithPath/ahmad.json";
+  static const nasaiPath = "$_hadithPath/nasai.json";
+  static const malikPath = "$_hadithPath/malik.json";
+  static const darimiPath = "$_hadithPath/darimi.json";
+  static const tirmidziPath = "$_hadithPath/tirmidzi.json";
+  static const abuDaudPath = "$_hadithPath/abu-daud.json";
+  static const ibnuMajahPath = "$_hadithPath/ibnu-majah.json";
+  //////////////////////////////////////////////////////
+  static const quranSvgPath = "$_imageAssetsPath/quran.svg";
+  static const compassSvgPath = "$_imageAssetsPath/compass.svg";
+  static const hadithSvgPath = "$_imageAssetsPath/hadith.svg";
+  static const seb7aSvgPath = "$_imageAssetsPath/seb7a.svg";
+  static const alarmSvgPath = "$_imageAssetsPath/alarm.svg";
   ////////////////////////////////////////////////////////
-  static const compassPath = "$imageAssetsPath/compass.png";
-  static const neddleKabahPath = "$imageAssetsPath/qibla.png";
-  static const dayImagePath = "$imageAssetsPath/day.jpg";
-  static const nightImagePath = "$imageAssetsPath/night.jpg";
-  static const quranBorder = "$imageAssetsPath/border.png";
-  
+  static const compassPath = "$_imageAssetsPath/compass.png";
+  static const neddleKabahPath = "$_imageAssetsPath/qibla.png";
+  static const dayImagePath = "$_imageAssetsPath/day.jpg";
+  static const nightImagePath = "$_imageAssetsPath/night.jpg";
+  static const quranBorder = "$_imageAssetsPath/border.png";
+
+  ////////////////////////////////////////////////////////
+  static const clickSoundPath = "$_soundsAssetsPath/click.mp3";
 }
