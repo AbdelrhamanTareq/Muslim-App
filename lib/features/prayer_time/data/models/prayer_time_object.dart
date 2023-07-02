eef1aafa9e7d93cc3fed51ed3cf4317d073b8a17
AbdelrhamanTareq <abdo_tarek01@yahoo.com>
2023-07-02 16:18:23 +0300

    add prayer times object

---
+++ lib/features/prayer_time/data/models/prayer_time_object.dart
@@ -0,0 +1,9 @@
+import 'package:geocoding/geocoding.dart';
+
+class PrayerTimeObjcet {
+  final Placemark? address;
+  final String? lat;
+  final String? long;
+
+  PrayerTimeObjcet({this.address, this.lat,this.long});
+}
