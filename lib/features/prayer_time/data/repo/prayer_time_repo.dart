8dc8d9552f7c838cb54270f00f3c6cfb0f2bea10
AbdelrhamanTareq <abdo_tarek01@yahoo.com>
2023-07-02 16:19:23 +0300

    add get prayer times from repo impl

--- lib/features/prayer_time/data/repo/prayer_time_repo.dart
+++ lib/features/prayer_time/data/repo/prayer_time_repo.dart
@@ -7,6 +7,8 @@ import 'package:muslim_app/features/prayer_time/data/network/app_api.dart';
 
 abstract class PrayerTimeRepo {
   Future<Either<Faliure, Map<int, Timings>>> getPrayerTimeData();
+  Future<Either<Faliure, Map<int, Timings>>> getPrayerTimeDataByLatLong(
+      {required String lat, required String long});
 }
 
 class PrayerTimeRepoImpl extends PrayerTimeRepo {
@@ -19,7 +21,6 @@ class PrayerTimeRepoImpl extends PrayerTimeRepo {
     final int year = DateTime.now().year;
     final int month = DateTime.now().month;
     try {
-      // final data = _prayerTimeLocalDate.getMonthPrayerTimesCacheData();
       final now = DateTime.now();
       var newDate = DateTime(now.year, now.month, now.day, 09, 01, 01)
               .millisecondsSinceEpoch /
@@ -54,4 +55,48 @@ class PrayerTimeRepoImpl extends PrayerTimeRepo {
       return Left(ServerFailure(e.toString()));
     }
   }
-}
+
+  @override
+  Future<Either<Faliure, Map<int, Timings>>> getPrayerTimeDataByLatLong(
+      {required String lat, required String long}) async {
+    final int year = DateTime.now().year;
+    final int month = DateTime.now().month;
+
+    try {
+      final now = DateTime.now();
+      var newDate = DateTime(now.year, now.month, now.day, 09, 01, 01)
+              .millisecondsSinceEpoch /
+          1000;
+      var newDateWithoutFractional = newDate.toStringAsFixed(0);
+      int newIntDate = int.parse(newDateWithoutFractional);
+      final data = _prayerTimeLocalDate.getMonthPrayerTimesLocalDataMap();
+      if (data[newIntDate] != null) {
+        return Right(data);
+      } else {
+        try {
+          final data = await _appServiceClient.getPrayerTimeDataByLatLong(
+              year, month, lat, long);
+          Map<int, Timings> prayerTimesMap = {};
+          for (var element in data.data) {
+            _prayerTimeLocalDate.setMonthPrayerTimesLocalData(
+                key: element.date.timestamp, prayerTimes: element.timings);
+            prayerTimesMap
+                .addAll({int.parse(element.date.timestamp): element.timings});
+          }
+
+          return Right(prayerTimesMap);
+        } catch (e) {
+          if (e is DioException) {
+            return Left(ServerFailure.fromDioError(e));
+          }
+          return Left(ServerFailure(e.toString()));
+        }
+      }
+    } catch (e) {
+      print(e.toString());
+      return Left(ServerFailure(e.toString()));
+    }
+ 
+  }
+} 
+
