97e1b37fd52c7fd13e6fdb1f49632a6a8d38eb93
AbdelrhamanTareq <abdo_tarek01@yahoo.com>
2023-07-02 16:19:57 +0300

    edit prauer time cubit and states

--- lib/features/prayer_time/presentation/logic/cubit/prayer_time_cubit.dart
+++ lib/features/prayer_time/presentation/logic/cubit/prayer_time_cubit.dart
@@ -1,48 +1,134 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 
 import 'package:muslim_app/features/prayer_time/data/models/prayer_time.dart';
 import 'package:muslim_app/features/prayer_time/data/repo/prayer_time_repo.dart';
 
+import '../../../../../core/data/app_local_data.dart';
+import '../../../../../core/function.dart';
+import '../../../../../core/injection_container.dart';
+import '../../../../../core/themes/app_colors.dart';
+
 part 'prayer_time_state.dart';
 
 class PrayerTimeCubit extends Cubit<PrayerTimeState> {
-  PrayerTimeCubit(
-    this._prayerTimeRepo,
-  ) : super(PrayerTimeInitial());
+  PrayerTimeCubit(this._prayerTimeRepo) : super(const PrayerTimeState());
 
   final PrayerTimeRepo _prayerTimeRepo;
 
   getPrayerTimeData() async {
-    emit(GetPrayerTimeDataLoading());
+    emit(state.copyWith(isLoading: true));
     final dataOrError = await _prayerTimeRepo.getPrayerTimeData();
     dataOrError.fold(
       (error) {
-        print(error.errorMessage);
-        emit(GetPrayerTimeDataError(error.errorMessage));
+        //print(error.errorMessage);
+        emit(state.copyWith(error: error.errorMessage, isLoading: false));
       },
       (data) {
         emit(
-          GetPrayerTimeDataSuccess(data),
+          state.copyWith(data: data, isLoading: false),
         );
       },
     );
   }
 
-  // getPrayerTimeData() async {
-  //   emit(GetPrayerTimeDataLoading());
-  //   final dataOrError = await _prayerTimeRepo.getPostsData();
-  //   dataOrError.fold(
-  //     (error) {
-  //       print(error.errorMessage);
-  //       emit(GetPrayerTimeDataError(error.errorMessage));
-  //     },
-  //     (data) {
-  //       emit(
-  //       GetPrayerTimeDataSuccess(data),
-  //     );
-  //     },
-  //   );
-  // }
+  getPrayerTimeDataByLocation(
+      {required String lat, required String long}) async {
+    // final List<String> list = instance<AppLocalData>().getLatAndLong() ?? [];
+    // String latttuide = (list[0].isNotEmpty) ? list[0] : lat!;
+    // String longitude = (list[0].isNotEmpty) ? list[1] : long!;
+    emit(state.copyWith(isLoading: true));
+    final dataOrError =
+        await _prayerTimeRepo.getPrayerTimeDataByLatLong(lat: lat, long: long);
+    dataOrError.fold(
+      (error) {
+        //print(error.errorMessage);
+        emit(state.copyWith(error: error.errorMessage, isLoading: false));
+      },
+      (data) {
+        emit(
+          state.copyWith(data: data, isLoading: false),
+        );
+      },
+    );
+  }
+
+  Future<bool> _handleLocationPermission() async {
+    //TODO locale msg
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      showToast('Location services are disabled. Please enable the services',
+          color: AppColors.error);
+      emit(state.copyWith(
+          error: "Location services are disabled. Please enable the services",
+          isLoadingGetLocation: false));
+      return false;
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        showToast('Location permissions are denied', color: AppColors.error);
+        emit(state.copyWith(
+            error: "Location permissions are denied",
+            isLoadingGetLocation: false));
+        return false;
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      showToast(
+          'Location permissions are permanently denied, we cannot request permissions.',
+          color: AppColors.error);
+      emit(state.copyWith(
+          error:
+              "Location permissions are permanently denied, we cannot request permissions.",
+          isLoadingGetLocation: false));
+      return false;
+    }
+    return true;
+  }
+
+  Future<void> getCurrentPosition() async {
+//TODO add internet connection checker
+    emit(state.copyWith(isLoadingGetLocation: true));
+    final hasPermission = await _handleLocationPermission();
+    if (!hasPermission) return;
+    try {
+      final position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+
+      final placemark = await getAddressFromLatLng(position);
+      // print(position.latitude);
+      // print(position.longitude);
+      instance<AppLocalData>()
+          .setLatAndLong(lat: position.latitude, long: position.longitude);
+      emit(state.copyWith(
+          position: position, isLoadingGetLocation: false, addres: placemark));
+      showToast("تم ايجاد الموقع");
+    } catch (e) {
+      emit(state.copyWith(error: e.toString(), isLoadingGetLocation: false));
+    }
+  }
+
+  Future<Placemark> getAddressFromLatLng(Position position) async {
+    List<Placemark> placemarks =
+        await placemarkFromCoordinates(position.latitude, position.longitude);
+    emit(state.copyWith(addres: placemarks[0]));
+    instance<AppLocalData>().setAddress(data: placemarks[0].toJson());
+    return placemarks[0];
+  }
+
+  getCity(String city) {
+    emit(state.copyWith(city: city));
+  }
+
+  getCountry(String country) {
+    emit(state.copyWith(country: country));
+  }
 }
--- lib/features/prayer_time/presentation/logic/cubit/prayer_time_state.dart
+++ lib/features/prayer_time/presentation/logic/cubit/prayer_time_state.dart
@@ -1,24 +1,83 @@
 part of 'prayer_time_cubit.dart';
 
-abstract class PrayerTimeState extends Equatable {
-  const PrayerTimeState();
+// abstract class PrayerTimeState extends Equatable {
+//   const PrayerTimeState();
 
-  @override
-  List<Object> get props => [];
-}
+//   @override
+//   List<Object> get props => [];
+// }
 
-class PrayerTimeInitial extends PrayerTimeState {}
+// class PrayerTimeInitial extends PrayerTimeState {}
 
-class GetPrayerTimeDataLoading extends PrayerTimeState {}
+// class GetPrayerTimeDataLoading extends PrayerTimeState {}
 
-class GetPrayerTimeDataSuccess extends PrayerTimeState {
-  final Map<int, Timings> data;
+// class GetPrayerTimeDataSuccess extends PrayerTimeState {
+//   final Map<int, Timings> data;
 
-  const GetPrayerTimeDataSuccess(this.data);
-}
+//   const GetPrayerTimeDataSuccess(this.data);
+// }
 
-class GetPrayerTimeDataError extends PrayerTimeState {
+// class GetLoactionByLocationServiceSucces2 extends PrayerTimeState {
+//   final Position position;
+
+//   const GetLoactionByLocationServiceSucces2(this.position);
+// }
+// class GetPrayerTimeDataError extends PrayerTimeState {
+//   final String error;
+
+//   const GetPrayerTimeDataError(this.error);
+// }
+
+class PrayerTimeState extends Equatable {
   final String error;
+  final bool isLoading;
+  final Position? position;
+  final Map<int, Timings> data;
+  final bool isLoadingGetLocation;
+  final Placemark? addres;
+  final String? country;
+  final String? city;
+  @override
+  List<Object?> get props => [
+        error,
+        isLoading,
+        position,
+        data,
+        isLoadingGetLocation,
+        addres,
+        country,
+        city
+      ];
+  const PrayerTimeState({
+    this.error = "",
+    this.isLoading = false,
+    this.position,
+    this.data = const {},
+    this.isLoadingGetLocation = false,
+    this.addres,
+    this.city,
+    this.country,
+  });
 
-  const GetPrayerTimeDataError(this.error);
+  PrayerTimeState copyWith({
+    String? error,
+    bool? isLoading,
+    Position? position,
+    Map<int, Timings>? data,
+    bool? isLoadingGetLocation,
+    Placemark? addres,
+    String? country,
+    String? city,
+  }) {
+    return PrayerTimeState(
+      error: error ?? this.error,
+      isLoading: isLoading ?? this.isLoading,
+      position: position ?? this.position,
+      data: data ?? this.data,
+      isLoadingGetLocation: isLoadingGetLocation ?? this.isLoadingGetLocation,
+      addres: addres ?? this.addres,
+      country: country ?? this.country,
+      city: city ?? city,
+    );
+  }
 }
