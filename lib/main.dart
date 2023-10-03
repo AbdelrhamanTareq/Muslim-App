0e9acced0c3dd6af7c1532b93433b47e2164a56b
AbdelrahmanTareq <abdo_tarek01@yahoo.com>
2023-10-03 19:48:13 +0300

    add new hive boxes

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:muslim_app/core/function.dart';
 import 'package:muslim_app/core/utils/app_notifications.dart';
+import 'package:muslim_app/features/hadith/data/local_data/hadith_local_data.dart';
 import 'package:muslim_app/features/prayer_time/data/models/prayer_time.dart';
+import 'package:muslim_app/features/quran/data/local_data/quran_local_data.dart';
 
 import 'app.dart';
 import 'core/bloc_observer.dart';
@@ -14,8 +16,12 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await initGetIt();
+  // initialize Hive
   await Hive.initFlutter();
-  await Hive.openBox(bookmarksKey);
+  // open hadith box
+  await Hive.openBox(hadithHiveBox);
+  // open quran box
+  await Hive.openBox(quranHiveBox);
   await AppNotification().initialize();
   Hive.registerAdapter(TimingsAdapter());
   await Hive.openBox(prayerTimesKey);
