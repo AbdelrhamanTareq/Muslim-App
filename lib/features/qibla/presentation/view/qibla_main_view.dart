ec7e226b9e2b295841f206fc5a59478a78af97c5
AbdelrahmanTareq <abdo_tarek01@yahoo.com>
2023-10-03 19:51:52 +0300

    test new compass update

--- lib/features/qibla/presentation/view/qibla_main_view.dart
+++ lib/features/qibla/presentation/view/qibla_main_view.dart
@@ -14,22 +14,12 @@ class _QiblaMainViewState extends State<QiblaMainView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     // backgroundColor: Colors.black45,
+      // backgroundColor: Colors.black45,
       appBar: AppBar(
         title: const Text(AppStrings.qibla),
       ),
       body: Center(
         child: SmoothCompass(
-          compassSupportText: AppStrings.compassSupportText,
-          allowLocationText: AppStrings.allowLocationText,
-          allowLocationFromSettingsAndPrivacyText:
-              AppStrings.allowLocationFromSettingsAndPrivacyText,
-          allowPermissionText: AppStrings.allowLocationText,
-          openSettingsText: AppStrings.openSettingsText,
-          enableLocationText: AppStrings.enableLocationText,
-          locationServicesDisabledText: AppStrings.locationServicesDisabledText,
-          locationServicesPermentDeniedText:
-              AppStrings.locationServicesPermentDeniedText,
           isQiblahCompass: true,
           compassBuilder: (context, compassData, compassAsset) {
             return AnimatedRotation(
@@ -70,6 +60,57 @@ class _QiblaMainViewState extends State<QiblaMainView> {
             );
           },
         ),
+        // child: SmoothCompass(
+        //   compassSupportText: AppStrings.compassSupportText,
+        //   allowLocationText: AppStrings.allowLocationText,
+        //   allowLocationFromSettingsAndPrivacyText:
+        //       AppStrings.allowLocationFromSettingsAndPrivacyText,
+        //   allowPermissionText: AppStrings.allowLocationText,
+        //   openSettingsText: AppStrings.openSettingsText,
+        //   enableLocationText: AppStrings.enableLocationText,
+        //   locationServicesDisabledText: AppStrings.locationServicesDisabledText,
+        //   locationServicesPermentDeniedText:
+        //       AppStrings.locationServicesPermentDeniedText,
+        //   isQiblahCompass: true,
+        //   compassBuilder: (context, compassData, compassAsset) {
+        //     return AnimatedRotation(
+        //       turns: compassData?.data?.turns ?? 0 / 360,
+        //       duration: const Duration(milliseconds: 400),
+        //       child: SizedBox(
+        //         height: MediaQuery.of(context).size.height * 0.5,
+        //         width: MediaQuery.of(context).size.width * 0.9,
+        //         child: Stack(
+        //           children: [
+        //             Positioned(
+        //               top: 0,
+        //               left: 0,
+        //               right: 0,
+        //               bottom: 0,
+        //               child: Image.asset(
+        //                 AppAssets.compassPath,
+        //                 fit: BoxFit.fill,
+        //               ),
+        //             ),
+        //             Positioned(
+        //               top: -20,
+        //               left: 50,
+        //               right: 0,
+        //               bottom: 40,
+        //               child: AnimatedRotation(
+        //                 turns: (compassData?.data?.qiblahOffset ?? 0) / 360,
+        //                 duration: const Duration(milliseconds: 400),
+        //                 child: Image.asset(
+        //                   AppAssets.neddleKabah1Path,
+        //                   fit: BoxFit.contain,
+        //                 ),
+        //               ),
+        //             ),
+        //           ],
+        //         ),
+        //       ),
+        //     );
+        //   },
+        // ),
       ),
     );
   }
