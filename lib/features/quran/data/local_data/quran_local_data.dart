4460baf1d06f337faf44f27a9c1fff273715c901
AbdelrahmanTareq <abdo_tarek01@yahoo.com>
2023-10-03 19:46:59 +0300

    edit quran local data

--- lib/features/quran/data/local_data/quran_local_data.dart
+++ lib/features/quran/data/local_data/quran_local_data.dart
@@ -1,12 +1,21 @@
+import 'package:hive/hive.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-abstract class QuranLocalData {
-  setBookmark({required double value, required String key});
-  double? getBookmark(String key);
-  setBookMarkedName({required String name, required String key});
-  String? getBookMarkedName(String key);
+const String quranHiveBox = "QURAN_BOX";
 
-  
+abstract class QuranLocalData {
+  // setBookmark({required double value, required String key});
+  // double? getBookmark(String key);
+  // setBookMarkedName({required String name, required String key});
+  // String? getBookMarkedName(String key);
+
+  Future<void> setQuranBookmarkedNames({
+    required String key,
+    required dynamic value,
+  });
+
+  getQuranBookmarkedNames(String key);
+
   double getQuranTextSize();
   Future<bool> setQuranTextSize(double value);
 }
@@ -17,25 +26,16 @@ class QuranLocalDataImpl extends QuranLocalData {
   final SharedPreferences _sharedPreferences;
 
   QuranLocalDataImpl(this._sharedPreferences);
-  @override
-  double? getBookmark(String key) {
-    return _sharedPreferences.getDouble(key);
-  }
 
-  @override
-  setBookmark({required double value, required String key}) async {
-    return await _sharedPreferences.setDouble(key, value);
-  }
+  // @override
+  // String? getBookMarkedName(String key) {
+  //   return _sharedPreferences.getString(key);
+  // }
 
-  @override
-  String? getBookMarkedName(String key) {
-    return _sharedPreferences.getString(key);
-  }
-
-  @override
-  setBookMarkedName({required String name, required String key}) async {
-    return await _sharedPreferences.setString(key, name);
-  }
+  // @override
+  // setBookMarkedName({required String name, required String key}) async {
+  //   return await _sharedPreferences.setString(key, name);
+  // }
 
   // EDITED
   @override
@@ -46,5 +46,16 @@ class QuranLocalDataImpl extends QuranLocalData {
   @override
   Future<bool> setQuranTextSize(double value) async {
     return await _sharedPreferences.setDouble(quranTextSizeKey, value);
+  }
+
+  @override
+  getQuranBookmarkedNames(String key) {
+    return Hive.box(quranHiveBox).get(key);
+  }
+
+  @override
+  Future<void> setQuranBookmarkedNames(
+      {required String key, required value}) async {
+    await Hive.box(quranHiveBox).put(key, value);
   }
 }
