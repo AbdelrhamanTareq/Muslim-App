8e954aef527234da36a277f8dc0e7d0043bd7286
abdelrhamanTareq <abdo_tarek01@yahoo,com>
2023-10-18 08:55:38 +0300

    #

--- lib/core/utils/app_extensions.dart
+++ lib/core/utils/app_extensions.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 extension ThemeShortcut on BuildContext {
+  get theme => Theme.of(this);
   get textThmem => Theme.of(this).textTheme;
   get bodyMedium => textThmem.bodyMedium;
 }
