612526a2a00a996cd9a8e61894d900a85a41a63e
abdelrhamanTareq <abdo_tarek01@yahoo,com>
2023-10-18 17:41:23 +0300

    add more extenstions

--- lib/core/utils/app_extensions.dart
+++ lib/core/utils/app_extensions.dart
@@ -4,4 +4,8 @@ extension ThemeShortcut on BuildContext {
   get theme => Theme.of(this);
   get textThmem => Theme.of(this).textTheme;
   get bodyMedium => textThmem.bodyMedium;
+  get displayMedium => textThmem.displayMedium;
+  get scaffoldBackGroundColor => theme.scaffoldBackgroundColor;
+  get blackLightColor => theme.shadowColor;
+  get whiteLightColor => theme.splashColor;
 }
